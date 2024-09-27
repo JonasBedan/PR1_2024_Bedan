182b92556d3d3f89b06bf3fb3dfc9ef4080d12f2
JonasBedan <jonas.bedan@gmail.com>
2024-09-27 11:25:35 +0200

    kod-27.9.

---
+++ C#/projekt1A.cs
@@ -0,0 +1,29 @@
+ namespace projekt1A
+{
+   internal class projekt1A.Program
+   {
+     static void Main(string[] args)
+     { 
+        /*Console.WriteLine("ahoj uzivateli");
+        int a;
+        a = 5;
+        Console.WriteLine("prrazena hodnota je"+a);
+        Console.ReadKey();
+        */
+
+        /*Console.WriteLine("Ahoj uživateli, jsem papoušek Lora.");
+        Console.Write("Zadej mi text k opkaování: ");
+        string text;
+        text = Console.ReadLine();
+        Console.WriteLine(text+" "+text);
+        Console.ReadKey();*/
+
+        Console.WriteLine("zadej hodnotu k vynasobeni");
+        int hodnota;//definice
+        hodnota = int.Parse(Console.WriteLine());
+        int vysledek = hodnota*2;//deklarace
+        Console.WriteLine("vysledek je: "+vysledek);   
+        
+     }
+   } 
+}
