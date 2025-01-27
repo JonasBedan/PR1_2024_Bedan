5e5772e53295e5858635e50b630223eb7edcb694
JonasBedan <jonas.bedan@gmail.com>
2025-01-27 09:49:42 +0100

    kamaradi

---
+++ okruh6/done_kamaradi/done_kamaradi/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace done_kamaradi
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Kamaradi("Anička", "Karel");
+        }
+
+        //sem přijde váš kód
+        public static void Kamaradi(string name, string name2) 
+        {
+            Console.WriteLine($"{name} a {name2} jsou kamarádi.");
+        }
+    }
+}
