f2fb1f7cc041caedfb7883bdb6679e9a265a2842
JonasBedan <jonas.bedan@gmail.com>
2025-01-27 10:30:24 +0100

    vymena

---
+++ okruh6/vymena1/vymena1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace vymena1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int cislo;
+            int cislo2 = generator();
+            Console.WriteLine("myslím si číslo, uhodneš ho?");
+            do
+            {                
+                cislo = nacteni();
+                vyssi(cislo, cislo2);
+            } while (generator != nacteni);
+            
+        }
+
+        
+
+        public static int nacteni() 
+        {
+            int guess;
+            do {
+                Console.Write("Jaké číslo si myslím: ");
+            } while (!int.TryParse(Console.ReadLine(), out guess));
+            return guess;
+        }
+        public static int generator() 
+        {
+            Random random = new Random();
+            int cislo = random.Next(0,101);
+            return cislo;
+        }
+        public static void vyssi(int guess, int cislo) 
+        {
+             
+            if(guess > cislo) 
+            {
+                Console.WriteLine("číslo je nižší.");
+            }
+            else if (guess < cislo) 
+            {
+                Console.WriteLine("číslo je nižší.");
+            }
+            else
+            {
+                Console.WriteLine("!!!UHODL JSI!!!");
+            }
+            
+        }
+
+    }
+}
---
+++ okruh6/vymena2/vymena2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace vymena2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int cislo = Generovani_cisel();
+            int cislo2;
+            Console.WriteLine("Mylím si číslo, dokážeš ho uhodnout?");
+            
+            do {
+                cislo2 = nacteni();
+                porovnani(cislo2, cislo);
+            }while (cislo!=cislo2);
+        }
+
+
+        public static int Generovani_cisel()
+        {
+            Random generator = new Random();
+            int vygenerovane_cislo = generator.Next(0, 101);
+
+            return vygenerovane_cislo;
+        }
+
+        public static void porovnani(int guess, int Generovani_cisel)
+        {
+            if (guess < Generovani_cisel)
+            {
+                Console.WriteLine("Vyšší");
+            }
+            else if (guess > Generovani_cisel) 
+            { 
+                Console.WriteLine("Nižší"); 
+            }
+            else 
+            {
+                Console.WriteLine("Uhodl");
+            }
+
+        }
+
+        public static int nacteni() 
+        {
+            int guess;
+            do
+            {
+                Console.Write("Jaké číslo si myslím: ");
+            } while (!int.TryParse(Console.ReadLine(), out guess));
+            return guess;
+        }
+    }
+}
