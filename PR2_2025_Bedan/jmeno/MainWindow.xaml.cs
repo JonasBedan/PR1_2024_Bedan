692651a07d4d2b2e390416e88689b26ddc42ac77
JonasBedan <jonas.bedan@gmail.com>
2026-01-19 11:21:14 +0100

    kalkulacka

--- PR2_2025_Bedan/jmeno/MainWindow.xaml.cs
+++ PR2_2025_Bedan/jmeno/MainWindow.xaml.cs
@@ -16,6 +16,10 @@ namespace jmeno
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double cislo = 0;
+        private double cislo1 = 0;
+        private double vysledek = 0;
+        private bool plus_clicked = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -24,7 +28,39 @@ namespace jmeno
         private void btnpozdrav_Click(object sender, RoutedEventArgs e)
         {
             string uzivatel = jmeno.Text;
-            lblpozdrav.Content ="ahoj " + uzivatel;
+            lblpozdrav.Content = "ahoj " + uzivatel;
+        }
+
+        private void btplus_Click(object sender, RoutedEventArgs e)
+        {
+            calc.Content = cislo+"+";
+            cislo1 = cislo;
+            cislo = 0;
+            plus_clicked = true;
+        }
+
+        private void btequals_Click(object sender, RoutedEventArgs e)
+        {
+            double vysledek = cislo + cislo1;
+            calc.Content = cislo1 + "+" + cislo + "=" + vysledek;
+            plus_clicked = false;
+        }
+
+        private void Cislicko_Click(object sender, RoutedEventArgs e)
+        {
+            Button cudlik = sender as Button;
+            int number = int.Parse(cudlik.Content.ToString());
+
+            cislo = cislo * 10 + number;
+
+            if (plus_clicked == true)
+            {
+                calc.Content = cislo1 + "+" + cislo;
+            }
+            else
+            {
+                calc.Content = cislo;
+            }
         }
     }
 }
